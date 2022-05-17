b5aaaa48d1f70cdcdd15b3fd3825b29f4ee72f34
Dan Lynch <pyramation@gmail.com>
2022-05-16 18:47:04 -0700

    scripts

---
+++ scripts/gen-proto.js
@@ -0,0 +1,22 @@
+import { join, resolve } from 'path';
+import telescope from '@osmonauts/telescope';
+import { camel, snake } from 'case';
+
+const protoDir = resolve(join(__dirname, '..', 'proto'));
+const outPath = resolve(__dirname, '..', 'src', 'proto');
+
+telescope({
+  protoDir,
+  outPath,
+  plugins: [
+    {
+      name: 'aminoCasing',
+      plugin: ({ protoPackage }) => {
+        if (protoPackage.startsWith('osmosis')) {
+          return camel;
+        }
+        return snake;
+      }
+    }
+  ]
+});
