6d82622ed263e47a642e829ec5540099c7d59186
Dan Lynch <pyramation@gmail.com>
2022-05-23 14:52:51 -0700

    protoDirs

--- scripts/gen-proto.js
+++ scripts/gen-proto.js
@@ -1,10 +1,14 @@
-import { join, resolve } from 'path';
+import { join } from 'path';
 import telescope from '@osmonauts/telescope';
 
-const protoDir = resolve(join(__dirname, '/../proto'));
-const outPath = resolve(join(__dirname, '../src/proto'));
+const protoDirs = [join(__dirname, '/../proto')];
+const outPath = join(__dirname, '../src/proto');
 
 telescope({
-  protoDir,
-  outPath
+  protoDirs,
+  outPath,
+  options: {
+    includeAminos: true,
+    includeLCDClient: false
+  }
 });
