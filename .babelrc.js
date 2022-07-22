308f3b35aea0ae8303b1e01598bb28863b8ffccf
Dan Lynch <pyramation@gmail.com>
2022-07-21 18:00:52 -0700

    pkg

--- .babelrc.js
+++ .babelrc.js
@@ -7,6 +7,8 @@ module.exports = (api) => {
       ['@babel/transform-runtime', { useESModules }],
       '@babel/proposal-object-rest-spread',
       '@babel/proposal-class-properties',
+      '@babel/plugin-proposal-nullish-coalescing-operator',
+      '@babel/plugin-proposal-optional-chaining',
       '@babel/plugin-proposal-numeric-separator',
       '@babel/proposal-export-default-from'
     ],
